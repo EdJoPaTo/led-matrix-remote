74e49b082d97bbf80fbbf687a54b766519e01b85
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-12-28 01:06:43 +0100

    refactor(cli): improve cli logic

--- src/cli.rs
+++ src/cli.rs
@@ -9,20 +9,22 @@ pub struct RuntimeArguments {
 }
 
 pub fn build() -> App<'static, 'static> {
-    let generic_args = [Arg::with_name("verbose")
-        .short("v")
-        .long("verbose")
-        .help("Still show commands instead of omitting them")];
-
     App::new("LED Matrix Remote")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
+        .global_setting(AppSettings::ColoredHelp)
         .setting(AppSettings::SubcommandRequired)
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .global(true)
+                .help("Still show commands instead of omitting them"),
+        )
         .subcommand(
             SubCommand::with_name("http")
                 .about("Read from stdin how the led matrix should look and send it via HTTP")
-                .args(&generic_args)
                 .arg(
                     Arg::with_name("HTTP Server")
                         .short("s")
@@ -36,7 +38,6 @@ pub fn build() -> App<'static, 'static> {
         .subcommand(
             SubCommand::with_name("mqtt")
                 .about("Read from stdin how the led matrix should look and send it via MQTT")
-                .args(&generic_args)
                 .arg(
                     Arg::with_name("MQTT Server")
                         .short("h")
@@ -70,40 +71,32 @@ pub fn build() -> App<'static, 'static> {
 pub fn get_runtime_arguments() -> RuntimeArguments {
     let matches = build().get_matches();
 
-    if let Some(mqtt_and_http_matches) = matches
-        .subcommand_matches("mqtt")
-        .or_else(|| matches.subcommand_matches("http"))
-    {
-        let verbose = mqtt_and_http_matches.is_present("verbose");
+    let verbose = matches.is_present("verbose");
 
-        let sender: Box<dyn Sender> = if let Some(http_matches) = matches.subcommand_matches("http")
-        {
-            let server = http_matches
-                .value_of("HTTP Server")
-                .expect("HTTP Server could not be read from command line");
+    let sender: Box<dyn Sender> = if let Some(http_matches) = matches.subcommand_matches("http") {
+        let server = http_matches
+            .value_of("HTTP Server")
+            .expect("HTTP Server could not be read from command line");
 
-            Box::new(HttpSender::new(server))
-        } else if let Some(mqtt_matches) = matches.subcommand_matches("mqtt") {
-            let mqtt_host = mqtt_matches
-                .value_of("MQTT Server")
-                .expect("MQTT Host could not be read from command line");
+        Box::new(HttpSender::new(server))
+    } else if let Some(mqtt_matches) = matches.subcommand_matches("mqtt") {
+        let mqtt_host = mqtt_matches
+            .value_of("MQTT Server")
+            .expect("MQTT Host could not be read from command line");
 
-            let mqtt_port = mqtt_matches
-                .value_of("MQTT Port")
-                .and_then(|s| s.parse::<u16>().ok())
-                .expect("MQTT Port could not be read from command line");
+        let mqtt_port = mqtt_matches
+            .value_of("MQTT Port")
+            .and_then(|s| s.parse::<u16>().ok())
+            .expect("MQTT Port could not be read from command line");
 
-            let mqtt_base_topic = mqtt_matches
-                .value_of("MQTT Base Topic")
-                .expect("MQTT Base Topic could not be read from command line");
+        let mqtt_base_topic = mqtt_matches
+            .value_of("MQTT Base Topic")
+            .expect("MQTT Base Topic could not be read from command line");
 
-            Box::new(MqttSender::new(mqtt_host, mqtt_port, &mqtt_base_topic))
-        } else {
-            panic!("There has to be a subcommand http or mqtt");
-        };
-
-        RuntimeArguments { sender, verbose }
+        Box::new(MqttSender::new(mqtt_host, mqtt_port, &mqtt_base_topic))
     } else {
         panic!("There has to be a subcommand http or mqtt");
-    }
+    };
+
+    RuntimeArguments { sender, verbose }
 }
