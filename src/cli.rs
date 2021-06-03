a924a3c416d78809845ec141421156e95cc8ff5c
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-06-03 09:09:15 +0200

    refactor(cli): move argument handling to main

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,4 @@
-use crate::http::HttpSender;
-use crate::mqtt::MqttSender;
-use crate::sender::Sender;
 use clap::{App, AppSettings, Arg, SubCommand};
-
-pub struct RuntimeArguments {
-    pub sender: Box<dyn Sender>,
-    pub verbose: bool,
-}
 
 pub fn build() -> App<'static, 'static> {
     App::new("LED Matrix Remote")
@@ -66,37 +58,4 @@ pub fn build() -> App<'static, 'static> {
                         .default_value("espMatrix"),
                 ),
         )
-}
-
-pub fn get_runtime_arguments() -> RuntimeArguments {
-    let matches = build().get_matches();
-
-    let verbose = matches.is_present("verbose");
-
-    let sender: Box<dyn Sender> = if let Some(http_matches) = matches.subcommand_matches("http") {
-        let server = http_matches
-            .value_of("HTTP Server")
-            .expect("HTTP Server could not be read from command line");
-
-        Box::new(HttpSender::new(server))
-    } else if let Some(mqtt_matches) = matches.subcommand_matches("mqtt") {
-        let mqtt_host = mqtt_matches
-            .value_of("MQTT Server")
-            .expect("MQTT Host could not be read from command line");
-
-        let mqtt_port = mqtt_matches
-            .value_of("MQTT Port")
-            .and_then(|s| s.parse::<u16>().ok())
-            .expect("MQTT Port could not be read from command line");
-
-        let mqtt_base_topic = mqtt_matches
-            .value_of("MQTT Base Topic")
-            .expect("MQTT Base Topic could not be read from command line");
-
-        Box::new(MqttSender::new(mqtt_host, mqtt_port, &mqtt_base_topic))
-    } else {
-        panic!("There has to be a subcommand http or mqtt");
-    };
-
-    RuntimeArguments { sender, verbose }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use io::Write;
 use retry::retry;
 use std::io::{self, BufRead};
 
+use crate::http::HttpSender;
+use crate::mqtt::MqttSender;
+use crate::sender::Sender;
+
 mod cli;
 mod command;
 mod http;
@@ -10,21 +14,50 @@ mod sender;
 
 fn main() {
     let stdin = io::stdin();
-    let mut args = cli::get_runtime_arguments();
+
+    let matches = cli::build().get_matches();
+    let verbose = matches.is_present("verbose");
+    let mut sender: Box<dyn Sender> = match matches.subcommand() {
+        ("http", Some(http_matches)) => {
+            let server = http_matches
+                .value_of("HTTP Server")
+                .expect("HTTP Server could not be read from command line");
+
+            Box::new(HttpSender::new(server))
+        }
+        ("mqtt", Some(mqtt_matches)) => {
+            let mqtt_host = mqtt_matches
+                .value_of("MQTT Server")
+                .expect("MQTT Host could not be read from command line");
+
+            let mqtt_port = mqtt_matches
+                .value_of("MQTT Port")
+                .and_then(|s| s.parse::<u16>().ok())
+                .expect("MQTT Port could not be read from command line");
+
+            let mqtt_base_topic = mqtt_matches
+                .value_of("MQTT Base Topic")
+                .expect("MQTT Base Topic could not be read from command line");
+
+            Box::new(MqttSender::new(mqtt_host, mqtt_port, &mqtt_base_topic))
+        }
+        _ => unimplemented!("Unknown subcommand"),
+    };
+
     for line in stdin.lock().lines() {
         let line_text = line.expect("failed to read line");
 
         match command::parse(&line_text) {
             None => println!("{}", line_text),
             Some(command) => {
-                if args.verbose {
+                if verbose {
                     print!("{}  ", line_text);
                 }
 
                 retry(retry::delay::Fixed::from_millis(20).take(2), || {
-                    let result = args.sender.send(&command);
+                    let result = sender.send(&command);
 
-                    if args.verbose {
+                    if verbose {
                         #[allow(clippy::non_ascii_literal)]
                         if result.is_ok() {
                             println!("✓");
